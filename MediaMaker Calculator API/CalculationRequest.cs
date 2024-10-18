4ea9adfbd757ef380ce073ba9d2751e6663d4501
Harpreet Matharu <Harpreet_Matharu@steris.com>
2024-10-18 11:04:42 +0100

    Initial commit of calculator API

---
+++ MediaMaker Calculator API/CalculationRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace MediaMaker_Calculator_API
+{
+    public class CalculationRequest
+    {
+        public double FirstNumber { get; set; }
+        public double SecondNumber { get; set; }
+    }
+}
---
+++ MediaMaker Calculator API/Services/CalculatorService.cs
@@ -0,0 +1,17 @@
+﻿namespace MediaMaker_Calculator_API.Services
+{
+    public class CalculatorService : ICalculatorService
+    {
+        public double Add(double firstNumber, double secondNumber) => firstNumber + secondNumber;
+        public double Subtract(double firstNumber, double secondNumber) => firstNumber - secondNumber;
+        public double Multiply(double firstNumber, double secondNumber) => firstNumber * secondNumber;
+        public double Divide(double firstNumber, double secondNumber)
+        {
+            if (secondNumber == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return firstNumber / secondNumber;
+        }
+    }
+}
